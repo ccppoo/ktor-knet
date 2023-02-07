d2b58c74fc345254e2ee1d7e2adbe8b9259e67b4
ccppoo <shmishmi79@gmail.com>
2023-02-07 19:20:12 +0900

    update integrated features

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,13 +1,26 @@
 package com.example
 
+import com.example.model.config.SecurityConfig
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import java.io.File
+import java.nio.file.Paths
 
 // EngineMain 사용하는 경우 application.yaml을 사용
 // embeddedServer 의 경우 gradle에서 설정을 해야하는 경우가 있음
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+val Application.securityConfig : SecurityConfig
+    get() = SecurityConfig(
+    environment.config.property("jwt.secret").getString(),
+    environment.config.property("jwt.audience").getString(),
+    environment.config.property("jwt.realm").getString()
+)
+
+val Application.basePath : File
+    get() = File(Paths.get("").toAbsolutePath().toString())
 
 fun Application.module() {
     configureSerialization()
